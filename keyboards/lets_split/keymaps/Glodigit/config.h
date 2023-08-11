cd32c18aeba65b92799f43384d641efa26cf36d8
Glodigit <kdengelin@hotmail.com>
2023-08-11 17:57:40 +0100

    Copied keymap into QMK. Taipo crashes MCU.

--- keyboards/lets_split/keymaps/Glodigit/config.h
+++ keyboards/lets_split/keymaps/Glodigit/config.h
@@ -21,11 +21,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // VIAL
+#ifdef VIAL_ENABLE
 #define VIAL_KEYBOARD_UID {0x7E, 0x6F, 0x5B, 0x6B, 0x35, 0x21, 0x7C, 0x1D}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 //#define VIAL_COMBO_ENTRIES 4
+#endif
 
 // RGB
 //#define RGBLIGHT_ANIMATIONS
@@ -42,3 +44,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Taipo
 //#define TAIPO_TAP_TIMEOUT 150
 #define ONESHOT_TIMEOUT 1000
+
+// Unicode
+#define UNICODE_SELECTED_MODES UNICODE_MODE_WINDOWS
--- keyboards/lets_split/keymaps/Glodigit/taipo.h
+++ keyboards/lets_split/keymaps/Glodigit/taipo.h
@@ -10,7 +10,50 @@
 #endif
 
 enum taipo_keycode {
+    TP_TLP = SAFE_RANGE,
+    TP_TLR,
+    TP_TLM,
+    TP_TLI,
+    TP_BLP,
+    TP_BLR,
+    TP_BLM,
+    TP_BLI,
+    TP_LIT,
+    TP_LOT,
+    TP_TRP,
+    TP_TRR,
+    TP_TRM,
+    TP_TRI,
+    TP_BRP,
+    TP_BRR,
+    TP_BRM,
+    TP_BRI,
+    TP_RIT,
+    TP_ROT,
+    KC_LAYER0,
+    KC_LAYER1,
+    KC_LAYER2,
+    KC_LAYER3,
+    KC_MOD_GA,
+    KC_MOD_GC,
+    KC_MOD_GS,
+    KC_MOD_AC,
+    KC_MOD_AS,
+    KC_MOD_CS,
+    KC_MOD_GAC,
+    KC_MOD_GAS,
+    KC_MOD_GCS,
+    KC_MOD_ACS,
+    KC_MOD_GACS,
+};
+
+/*
+enum taipo_keycode {
+    #ifdef VIAL_ENABLE
     TP_TLP = QK_USER_0,
+    #else
+    TP_TLP = SAFE_RANGE,
+    #endif
     TP_TLR,
     TP_TLM,
     TP_TLI,
@@ -48,6 +91,8 @@ enum taipo_keycode_cont {
     KC_MOD_ACS,
     KC_MOD_GACS,
 };
+
+*/
 
 typedef struct {
     uint16_t keycode;
